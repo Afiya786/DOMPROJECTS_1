c5b532e94ea0153985010306844f9cf6769b2802
Afiya Ansari <75537823+Afiya786@users.noreply.github.com>
2024-02-13 11:17:33 +0530

    Add files via upload

---
+++ colorCHANGE.js
@@ -0,0 +1,31 @@
+const buttons =document.querySelectorAll('.button')
+const body = document.querySelector("body")
+
+buttons.forEach(button =>{
+    console.log(button)
+    //events are like refreshing page,mouse event , click event etc.
+    button.addEventListener("click", function(e){  //e is the name of event
+     console.log(e)
+     console.log(e.target)
+     if(e.target.id == "grey"){
+        body.style.backgroundColor = e.target.id;
+     }
+     if(e.target.id == "blue"){
+        body.style.backgroundColor = e.target.id;
+     }
+
+     if(e.target.id == "yellow"){
+        body.style.backgroundColor = e.target.id;
+     }
+
+     if(e.target.id == "white"){
+        body.style.backgroundColor = e.target.id;
+     }
+     if(e.target.id == "red"){
+        body.style.backgroundColor = e.target.id;
+     }
+
+
+
+    })
+})
